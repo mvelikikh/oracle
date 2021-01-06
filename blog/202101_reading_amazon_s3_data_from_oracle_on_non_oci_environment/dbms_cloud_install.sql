9af9b3107fe9f2d2179064e075e5519174236ba3
Mikhail Velikikh <mvelikikh@gmail.com>
2021-01-06 19:16:49 +0000

    http://mvelikikh.blogspot.com/2021/01/reading-amazon-s3-data-from-oracle-on.html

---
+++ blog/202101_reading_amazon_s3_data_from_oracle_on_non_oci_environment/dbms_cloud_install.sql
@@ -0,0 +1,21 @@
+spo /tmp/dbms_cloud
+
+@?/rdbms/admin/dbms_cloud_types.sql
+@?/rdbms/admin/dbms_cloud.sql
+@?/rdbms/admin/dbms_cloud_task_catalog.sql
+@?/rdbms/admin/dbms_cloud_catalog.sql
+@?/rdbms/admin/dbms_cloud_task_views.sql
+@?/rdbms/admin/dbms_cloud_capability.sql
+@?/rdbms/admin/prvt_cloud_core.plb
+@?/rdbms/admin/prvt_cloud_core_body.plb
+@?/rdbms/admin/prvt_cloud_internal.plb
+@?/rdbms/admin/prvt_cloud_request.plb
+@?/rdbms/admin/prvt_cloud_task.plb
+@?/rdbms/admin/prvt_cloud_task_body.plb
+@?/rdbms/admin/prvt_cloud_body.plb
+@?/rdbms/admin/prvt_cloud_capability_body.plb
+@?/rdbms/admin/prvt_cloud_internal_body.plb
+@?/rdbms/admin/prvt_cloud_request_body.plb
+@?/rdbms/admin/dbms_cloud_metadata.sql
+
+spo off
